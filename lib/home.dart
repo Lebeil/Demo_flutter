85f0ed7de47a4ab2a96d1fdb7358f76b19b2e552
Liedel <liedel75@gmail.com>
2022-01-10 15:05:52 +0100

    add toggle + opérateur ternaire

--- lib/home.dart
+++ lib/home.dart
@@ -9,9 +9,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var _score = 0;
+  var _afficherLeBouton = false;
   void _plusButtonClicked() {
     setState(() {
       _score = _score + 1;
+    });
+  }
+
+  void _toggleButton() {
+    setState(() {
+      _afficherLeBouton = !_afficherLeBouton;
     });
   }
 
@@ -24,8 +31,17 @@ class _HomeState extends State<Home> {
         IconButton(
             onPressed: _plusButtonClicked,
             iconSize: 50,
-            icon: const Icon(Icons.plus_one))
-      ],)
+            icon: const Icon(Icons.plus_one)),
+        ElevatedButton.icon(onPressed: _plusButtonClicked, icon: const Icon(Icons.plus_one), label: const Text("Ajouter un bouton")),
+        OutlinedButton(onPressed: _toggleButton, child: Text((_afficherLeBouton ? "Masquer " : "Afficher ") + "le bouton") //ternaire
+        ),
+        if(_afficherLeBouton)
+          TextButton(
+            onPressed: _plusButtonClicked,
+            child: const Text('Bouton'),
+          )
+      ],
+      ),
     );
   }
 }
